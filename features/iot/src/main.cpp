77212d3d45ca9312e126ebad9440bfa952ca69c0
chethack <techbuddy022@gmail.com>
2023-11-19 05:19:42 +0300

    added automated minting

--- features/iot/src/main.cpp
+++ features/iot/src/main.cpp
@@ -9,6 +9,7 @@
 #include <ArduinoJson.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <HTTPClient.h>
 
 #define SHT10_DATA_PIN  21
 #define SHT10_CLOCK_PIN 22
@@ -29,7 +30,7 @@ AsyncWebServer server(80);
 bool isMeasuring = false;
 unsigned long startTime = 0;
 unsigned long stopTime = 0;
-float lastEmission = 0;
+float totalEmissions = 0;
 
 struct CO2Reading {
   float concentration;
@@ -43,6 +44,7 @@ float calculateCO2Emissions(float co2_ppm, float flow_rate_m3s);
 void handleCO2Endpoint(AsyncWebServerRequest *request);
 void wifiServerTask(void *pvParameters);
 void measureCO2Task(void *pvParameters);
+void httpGetTask(void * parameter);
 
 void setup() {
   Serial.begin(115200);
@@ -79,16 +81,30 @@ void wifiServerTask(void *pvParameters) {
   Serial.println("Connected with IP: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/co2", HTTP_ANY, handleCO2Endpoint);
+  server.on("/co2", HTTP_GET, handleCO2Endpoint);
   server.begin();
 
   for (;;) {
-    // Keep the task alive
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
+  server.on("/triggerHttpRequest", HTTP_GET, [](AsyncWebServerRequest *request) {
+    // Start the HTTP request task when this endpoint is accessed
+    xTaskCreate(
+        httpGetTask,          /* Task function */
+        "HTTPGetTask",        /* Name of the task */
+        10000,                /* Stack size (in bytes) */
+        NULL,                 /* Parameter to pass to the task */
+        1,                    /* Task priority */
+        NULL);                /* Task handle */
+
+    // Respond to the HTTP request
+    request->send(200, "text/plain", "HTTP request triggered");
+});
+
 }
 
 void measureCO2Task(void *pvParameters) {
+  unsigned long lastSampleTime = 0;
   for (;;) {
     if (digitalRead(BUTTON_PIN) == LOW) {
       if (isMeasuring) {
@@ -106,13 +122,19 @@ void measureCO2Task(void *pvParameters) {
       delay(500); // Debounce delay
     }
 
-    if (isMeasuring) {
+    if (isMeasuring && millis() - lastSampleTime >= 2000) {
+      lastSampleTime = millis();
       CO2Reading reading;
       reading.concentration = mq135_sensor.getCorrectedPPM(sht10.readTemperatureC(), sht10.readHumidity());
       reading.timestamp = millis();
       co2Readings.push_back(reading);
 
-      if (reading.concentration < 1000) {
+      Serial.print("CO2 Concentration: ");
+      Serial.print(reading.concentration);
+      Serial.print(" ppm at ");
+      Serial.println(reading.timestamp);
+
+      if (reading.concentration < 200) {
         digitalWrite(GREEN_LED_PIN, HIGH);
         digitalWrite(YELLOW_LED_PIN, LOW);
         digitalWrite(RED_LED_PIN, LOW);
@@ -132,12 +154,12 @@ void measureCO2Task(void *pvParameters) {
 }
 
 void calculateAndDisplayResults() {
-  float totalEmissions = 0;
+  float lastEmission = 0;
   for (size_t i = 1; i < co2Readings.size(); i++) {
     float timeIntervalHours = (co2Readings[i].timestamp - co2Readings[i - 1].timestamp) / 3600000.0;
     float emission = calculateCO2Emissions(co2Readings[i].concentration, EXHAUST_FLOW_RATE) * timeIntervalHours;
     lastEmission = calculateCO2Emissions(co2Readings[i].concentration, EXHAUST_FLOW_RATE);
-    totalEmissions = lastEmission;
+    totalEmissions += lastEmission;
   }
 
   lcd.clear();
@@ -146,21 +168,47 @@ void calculateAndDisplayResults() {
   lcd.print(totalEmissions);
   lcd.print(" tonnes");
   co2Readings.clear();
+  xTaskCreate(
+        httpGetTask,          /* Task function. */
+        "HTTPGetTask",        /* String with name of task. */
+        10000,                /* Stack size in bytes. */
+        NULL,                 /* Parameter passed as input of the task */
+        1,                    /* Priority of the task. */
+        NULL);                /* Task handle. */
+
+    co2Readings.clear();
 }
 
 float calculateCO2Emissions(float co2_ppm, float flow_rate_m3s) {
   float mass_fraction = co2_ppm / 1000000.0;
   float density_of_exhaust = 1.204;
   float mass_of_CO2_per_second = flow_rate_m3s * density_of_exhaust * mass_fraction;
-  return mass_of_CO2_per_second * 3600 * 0.001 * 10000;
+  return mass_of_CO2_per_second * 3600 * 0.001 * 100;
 }
 
 void handleCO2Endpoint(AsyncWebServerRequest *request) {
     StaticJsonDocument<200> doc;
-    doc["message"] = "Latest CO2 emission reading SCALED BY 10000 for demo purposes";
-    doc["emission"] = lastEmission;
+    doc["message"] = "Latest CO2 emission reading SCALED BY 100000 for demo purposes";
+    doc["emission"] = totalEmissions;
 
     String response;
     serializeJson(doc, response);
     request->send(200, "application/json", response);
+}
+
+void httpGetTask(void * parameter){
+    HTTPClient http;
+    http.begin("http://e8f8-213-43-1-133.ngrok-free.app/sendRequest");
+    http.addHeader("ngrok-skip-browser-warning", "1"); 
+
+    int httpCode = http.GET(); // Perform the GET request
+    if(httpCode > 0) {
+        String payload = http.getString();
+        Serial.println(payload); // Print the response payload
+    } else {
+        Serial.println("Error on HTTP request");
+    }
+
+    http.end(); // Free the resources
+    vTaskDelete(NULL); // Delete the task when done
 }
